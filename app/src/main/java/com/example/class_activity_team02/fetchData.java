9ce97dc692021fb4a9b50fcb085a9297cdc111e4
1893713 <kevalbhaimiyani5@gmail.com>
2019-07-09 14:41:37 -0400

    chhelo pryas

---
+++ app/src/main/java/com/example/class_activity_team02/fetchData.java
@@ -0,0 +1,74 @@
+package com.example.class_activity_team02;
+
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Abhishek Panwar on 7/14/2017.
+ */
+
+public class fetchData extends AsyncTask<Void,Void,Void> {
+    private String key = "https://api.myjson.com/bins/j5f6b";
+    String data ="";
+    String dataParsed = "";
+    String singleParsed ="";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL(key);
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            JSONArray JA = new JSONArray(data);
+            for(int i =0 ;i <JA.length(); i++){
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed =  "Name:" + JO.get("name") + "\n"+
+                        "image:" + JO.get("image") + "\n"+
+                        "type:" + JO.get("type") + "\n"+
+                        "ability:" + JO.get("ability") + "\n"+
+                        "description:" + JO.get("description") + "\n"+
+                        "height:" + JO.get("height") + "\n";
+
+                dataParsed = dataParsed + singleParsed +"\n" ;
+
+
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        MainActivity.data.setText(this.dataParsed);
+
+    }
+}
+
