28f9b0696ec568ab852e5f85a7cf6b5ce770c066
1893713 <kevalbhaimiyani5@gmail.com>
2019-07-09 13:46:57 -0400

    bhaktti-splashscreenagain

---
+++ app/src/main/java/com/example/class_activity_team02/SplashScreenM.java
@@ -0,0 +1,49 @@
+package com.example.class_activity_team02;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreenM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAGS_CHANGED, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        logolauncher logolauncher =new logolauncher();
+        logolauncher.start();
+
+
+    }
+
+
+
+    private class logolauncher extends Thread{
+        public void run()
+        {
+            try{
+                sleep(1000*4);
+            }
+            catch (InterruptedException e)
+                    
+            {
+                e.printStackTrace();
+            }
+
+            Intent intent =new Intent(SplashScreenM.this,MainActivity.class);
+            startActivity(intent);
+
+            SplashScreenM.this.finish();
+        }
+    }
+
+
+}
